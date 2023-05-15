039ad2e63857ae37b00bf3419293fbb55308c01f
Eu! <soareskaique579@gamil.com>
2023-05-15 06:58:54 -0300

    Início de servlet de registro

--- src/java/servlet/Registro.java
+++ src/java/servlet/Registro.java
@@ -5,8 +5,14 @@
  */
 package servlet;
 
+import controle.Controle;
+import entidades.Conta;
+import java.util.Iterator;
+import java.util.List;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +23,13 @@ import javax.servlet.http.HttpServletResponse;
  * @author windows
  */
 public class Registro extends HttpServlet {
+    
+    private Controle con;
+    @Override
+    public void init() throws ServletException{                             
+            super.init();
+            con = new Controle();    
+                                              }
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -70,7 +83,76 @@ public class Registro extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String erroRegistro;  
+        Conta conta = new Conta(); 
+        List<Conta> lista;
+        
+        String login = request.getParameter("novoLogin");
+        String senha = request.getParameter("novaSenha");
+        
+        
+        request.getSession().setAttribute("listaContas", con.consultarContas());
+        lista = (List) request.getSession().getAttribute("listaContas");
+        
+        
+        Iterator<Conta> ite = lista.iterator();
+        int i = -1;
+        
+        
+        
+        while(ite.hasNext()){
+                            i++;
+                            conta = ite.next();
+                            if(conta.getLogin().equals(login)){
+                                
+                                erroRegistro = "Essa conta já existe! Use outro nome de usuário.";
+                                request.getSession().setAttribute("erroRegistro", erroRegistro);
+                                //redirect
+                                break;
+                                
+                                                              }}
+        i=0; 
+        
+        if(senha.length() < 10){
+            erroRegistro = "Essa senha é muito curta! Use pelo menos 10 caracteres";
+            request.getSession().setAttribute("erroRegistro", erroRegistro);
+                              }
+        
+        Pattern maisculas = Pattern.compile("[A-Z]");
+        Pattern minusculas = Pattern.compile("[a-z]");
+        Pattern numeros = Pattern.compile("[0-9]");
+        Pattern especiais = Pattern.compile ("[^A-Za-z0-9]");
+        
+        
+        Matcher temMaisculas = maisculas.matcher(senha);
+        Matcher temMinusculas = minusculas.matcher(senha);
+        Matcher temNumeros = numeros.matcher(senha);
+        Matcher temEspeciais = especiais.matcher(senha);
+        
+        if (temMaisculas.find() == false){
+            erroRegistro = "Essa senha não possuí caracteres maiúsculos! Formule outra que possua.";
+            request.getSession().setAttribute("erroRegistro", erroRegistro);
+            //redirect
+                                         }
+        
+        if (temMinusculas.find() == false){
+            erroRegistro = "Essa senha não possuí caracteres minúsculos! Formule outra que possua.";
+            request.getSession().setAttribute("erroRegistro", erroRegistro);
+                                          }
+        if (temNumeros.find() == false){
+            erroRegistro = "Essa senha não possuí caracteres numéricos! Formule outra que possua.";
+            request.getSession().setAttribute("erroRegistro", erroRegistro);
+                                       }
+        if (temEspeciais.find() == false){
+            erroRegistro = "Essa senha não possuí caracteres numéricos! Formule outra que possua.";
+            request.getSession().setAttribute("erroRegistro", erroRegistro);
+                                         }
+        
+        if(senha.equals(login)){
+            erroRegistro = "A senha deve ser diferente do login!";
+            request.getSession().setAttribute("erroRegistro", erroRegistro);                   
+                               }
+        
     }
 
     /**
